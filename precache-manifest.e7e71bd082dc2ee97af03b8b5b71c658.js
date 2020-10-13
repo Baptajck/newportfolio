32c71d7e484df0138788aaac5325869d7aa67e2d
Baptajck <parville.baptiste@gmail.com>
2020-10-13 09:46:45 +0200

    Updates

---
+++ precache-manifest.e7e71bd082dc2ee97af03b8b5b71c658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8df89665d3b1b496f0de6e0033a8cd17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ab35bba6fbd2718a35",
+    "url": "/static/css/main.c99d0df5.chunk.css"
+  },
+  {
+    "revision": "d14ae3f286a8071a2383",
+    "url": "/static/js/2.e1c33c61.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.e1c33c61.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24ab35bba6fbd2718a35",
+    "url": "/static/js/main.392f6c00.chunk.js"
+  },
+  {
+    "revision": "3b2bda409d83d3f997fa",
+    "url": "/static/js/runtime-main.2fc13a78.js"
+  }
+]);
